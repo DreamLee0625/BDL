25749c756d707debb7606c74215854c554bd0797
Xiang Li <lixiang_cn@foxmail.com>
2019-04-30 17:09:52 +0800

    init project

---
+++ BilibiliDownloader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BilibiliDownloader
+//
+//  Created by 李想 on 2019/4/30.
+//  Copyright © 2019 李想. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BilibiliDownloader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BilibiliDownloader
+//
+//  Created by 李想 on 2019/4/30.
+//  Copyright © 2019 李想. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
