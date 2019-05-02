86177428bc709d58eed6f5100976ed65d577d382
Xiang Li <lixiang_cn@foxmail.com>
2019-05-02 20:11:04 +0800

    get video_url and video_quality

--- BilibiliDownloader/ViewController.swift
+++ BilibiliDownloader/ViewController.swift
@@ -10,6 +10,24 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var input_url: NSTextField!
+    @IBOutlet weak var input_quality: NSComboBoxCell!
+    
+    @IBAction func begin_download(_ sender: Any) {
+        var video_url = input_url.stringValue
+        if video_url.isEmpty {
+            NSLog("error: no input_url")
+            // raise error 弹窗
+        }
+        var video_quality = input_quality.stringValue
+        if video_quality.isEmpty {
+            NSLog("error: select a quality")
+            // raise error 弹窗
+        }
+        NSLog("begin download from \(video_url)")
+        NSLog("\(video_quality)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
