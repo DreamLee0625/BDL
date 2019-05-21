10ffe0e713f9efdbb592250bd08ce390173306a1
Xiang Li <lixiang_cn@foxmail.com>
2019-05-21 22:07:07 +0800

    190512

---
+++ BilibiliDownloader/Models/BilibiliDownloader.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  BilibiliDownloader
+//
+//  Created by 李想 on 2019/5/2.
+//  Copyright © 2019 李想. All rights reserved.
+//
+
+import Foundation
---
+++ BilibiliDownloader/View Controllers/AboutViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AboutViewController.swift
+//  BilibiliDownloader
+//
+//  Created by 李想 on 2019/5/2.
+//  Copyright © 2019 李想. All rights reserved.
+//
+
+import Cocoa
+
+class AboutViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
